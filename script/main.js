3511e7b9d14be4e6a41ff79f9b530e953425f8e4
Eileen <eileen.santos@outlook.de>
2025-06-30 09:27:02 +0200

    Function deleteItem and Function openCart

--- script/main.js
+++ script/main.js
@@ -3,9 +3,6 @@ let cartData = {};
 let subtotal = 0;
 let deliveryCost = 2.5;
 
-
-
-
 function Init() {
     let donutContainer = document.getElementById('Donuts');
 
@@ -18,7 +15,7 @@ function Init() {
                     <p>${donut.description}</p>
                     <p>Price: ${donut.price.toFixed(2)}</p>
                 </div>
-                <div class="choiceContainer">
+                <div class="choiceContainer" onclick="openCart()">
                     <img src="./assets/icons/plus.png" 
                         alt="Add to cart" 
                         class="addToCart" 
@@ -37,6 +34,11 @@ function toggleCart() {
     cartData.classList.toggle('show');
 }
 
+function openCart() {
+    let cartData = document.getElementById('cart');
+    cartData.classList.add('show');
+}
+
 function addtoCart (index) {
 
     const donut = donuts[index];
@@ -52,23 +54,50 @@ function addtoCart (index) {
 
     subtotal += donut.price;
 
+    updateCart();
+}
+
+function updateCart() {
+
     let cartHTML = '';
-    for (let itemName in cartData) {
-        let item =  cartData[itemName];
-        let itemTotal = item.quantity * item.price;
-        cartHTML += `
-            <p>${itemName} x ${item.quantity} = ${itemTotal.toFixed(2)} €
-            <img src="./assets/icons/loschen.png" alt="Löschen" class="deleteIcon">
-            </p>`;
+
+    if (Object.keys(cartData).length === 0) {
+        cartHTML = '<p>Der Warenkorb ist leer!</p>';
+        document.getElementById('subtotal').textContent = '0.00 €';
+        document.getElementById('deliveryCost').textContent = '0.00 €';
+        document.getElementById('totalPrice').textContent = '0.00 €';
+    } else {
+        for (let itemName in cartData) {
+            let item = cartData[itemName];
+            let itemTotal = item.quantity * item.price;
+            cartHTML += `
+                <p>${itemName} x ${item.quantity} = ${itemTotal.toFixed(2)} €
+                <img src="./assets/icons/loschen.png" 
+                alt="Löschen"
+                class="deleteIcon" 
+                onclick="deleteItem('${itemName}');">
+                </p>
+            `;
+        }
+
+        document.getElementById('subtotal').textContent = `${subtotal.toFixed(2)} €`;
+        document.getElementById('deliveryCost').textContent = `${deliveryCost.toFixed(2)} €`;
+        const total = subtotal + deliveryCost;
+        document.getElementById('totalPrice').textContent = `${total.toFixed(2)} €`;
     }
 
     document.getElementById('cartItems').innerHTML = cartHTML;
-
-    document.getElementById('subtotal').textContent = `${subtotal.toFixed(2)} €`;
-
-    document.getElementById('deliveryCost').textContent = `${deliveryCost.toFixed(2)} €`;
-
-    const total = subtotal + deliveryCost;
-        document.getElementById('totalPrice').textContent = `${total.toFixed(2)} €`;
 }
 
+function deleteItem(itemName) {
+    
+    let item = cartData[itemName];
+
+    item.quantity -= 1;
+    subtotal -= item.price;
+    
+    if (item.quantity === 0) {
+        delete cartData[itemName];
+    }
+    updateCart();
+}
